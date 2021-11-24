4039f4eebd09b39f19832ae5c4026b9be9e42a78
Ishtiaq Mahmood Amin <ishtiaq.mahmood@protonmail.com>
2021-11-24 17:46:18 +0100

    Open count viewer fragment added

---
+++ app/src/main/java/ishtiaq/codingchallenge/randomjokes/view/OpenCountFragment.kt
@@ -0,0 +1,42 @@
+package ishtiaq.codingchallenge.randomjokes.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ishtiaq.codingchallenge.randomjokes.RandJokesApp
+import ishtiaq.codingchallenge.randomjokes.databinding.FragmentOpenCountBinding
+
+class OpenCountFragment : Fragment() {
+    // assign the _binding variable initially to null and
+    // also when the view is destroyed again it has to be set to null
+    private var _binding: FragmentOpenCountBinding? = null
+
+    // with the backing property of the kotlin we extract
+    // the non null value of the _binding
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentOpenCountBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.tvCount.text = RandJokesApp.openCount.toString()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            OpenCountFragment()
+    }
+}
